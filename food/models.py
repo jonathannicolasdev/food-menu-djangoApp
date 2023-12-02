3ee39af4fa79b30a6e50f45e232bd40a1ab9c6c4
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-12-02 18:56:11 -0500

    feat: adding image field to model

--- food/models.py
+++ food/models.py
@@ -9,4 +9,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp8MdrNj1NpuBB2kQuEmLxgqn9y3ZZWiAipWbkfzkLIN2DGTQ6U9A7LFQH_i5a1fYjtJg&usqp=CAU")
 
